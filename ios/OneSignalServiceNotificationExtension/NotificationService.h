08d1f1a084af07adb42cd4d2d3a373b7eabc17fe
William Shepherd <will@codeincolor.io>
2022-04-07 11:21:37 -0500

    Add project

---
+++ ios/OneSignalServiceNotificationExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  OneSignalServiceNotificationExtension
+//
+//  Created by William Shepherd on 3/30/22.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
